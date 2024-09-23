a809a09ee35e0e30b796d16c6bc60272467314ef
fernandomeca <fernando_mecca@hotmail.com>
2024-09-23 12:09:41 -0300

    criado projeto DesafioControleFluxo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,36 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+        
+        try {
+            contar(parametroUm, parametroDois);
+        
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("Erro: " + exception.getMessage());
+        }
+        
     }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+       
+                
+        if (parametroUm > parametroDois){
+            System.out.println("Atenção! O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+
+    private static class ParametrosInvalidosException extends Exception {
+
+    }
+    
 }
